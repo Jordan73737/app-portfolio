3fef37411285ea809c45e7af251cfdde1f0a34f2
Jordan73737 <jordanmctaggart7@hotmail.co.uk>
2023-06-25 13:24:10 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Shrek")
+    content = """
+    Hello there! I'm Shrek
+    """
+    st.write(content)
